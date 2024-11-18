3074e071c12fcf9763751fe3fb5ec10f1a661037
MarkosComK <markoscomks@gmail.com>
2024-11-18 15:13:15 +0000

    Ex 00 easy?

---
+++ CPP_00/ex00/example.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   example.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marsoare <marsoare@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/18 13:28:08 by marsoare          #+#    #+#             */
+/*   Updated: 2024/11/18 15:12:33 by marsoare         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main(int argc, char **argv)
+{
+	if (argc < 2)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (0);
+	}
+	for (int i = 1; argv[i]; i++)
+	{
+		for (int j = 0; argv[i][j]; j++)
+		{
+			std::cout << (char )toupper(argv[i][j]);
+		}
+	}
+	std::cout << std::endl;
+	return (0);
+}
---
+++ CPP_00/ex00/notes.md
@@ -0,0 +1,13 @@
+Class names need to be written in **UpperCamelCase** according to the subject
+
+---
+
+Write class names in UpperCamelCase format. Files containing class code will
+always be named according to the class name. For instance:
+ClassName.hpp/ClassName.h, ClassName.cpp, or ClassName.tpp. Then, if you
+have a header file containing the definition of a class "BrickWall" standing for a
+brick wall, its name will be BrickWall.hpp.
+
+We can use everything from standard library of C++ (YEAH!). 
+
+We should get rid of leaks too in C++. Then, be aware of leaks using **new** 
