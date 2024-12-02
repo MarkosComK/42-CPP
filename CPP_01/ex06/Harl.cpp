d25c2f96956e652a5c33cf1109f7900c6d195373
MarkosComK <markoscomks@gmail.com>
2024-12-02 16:26:15 +0000

    Fixes on ex06

--- CPP_01/ex06/Harl.cpp
+++ CPP_01/ex06/Harl.cpp
@@ -21,19 +21,87 @@ void	Harl::filter(std::string level)
 
 	switch (index)
 	{
-		case 0:
+		case (0):
 			printLevel("DEBUG", 0);
-			[[fallthrough]];
-		case 1:
+			break;
+		case (1):
 			printLevel("INFO", 1);
-			[[fallthrough]];
-		case 2:
+			break;
+		case (2):
 			printLevel("WARNING", 2);
-			[[fallthrough]];
-		case 3:
+			break;
+		case (3):
 			printLevel("ERROR", 3);
 			break;
 		default:
 			std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
     }
+}
+
+//previous exercise
+void	Harl::debug( void )
+{
+	std::cout <<
+	"love having extra bacon for my 7XL-double-cheese-triple-pickle-speciaketchup burger. I really do!"
+	<< std::endl;
+}
+
+void	Harl::info( void )
+{
+	std::cout <<
+	"cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!"
+	<< std::endl;
+}
+
+void	Harl::warning( void )
+{
+	std::cout <<
+	"think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month."
+	<< std::endl;
+}
+
+void	Harl::error( void )
+{
+	std::cout <<
+	"This is unacceptable! I want to speak to the manager now."
+	<< std::endl;
+}
+
+Harl::Harl()
+{
+    // Initialize arrays
+	levels[0] = "DEBUG";
+	levels[1] = "INFO";
+	levels[2] = "WARNING";
+	levels[3] = "ERROR";
+
+	f[0] = &Harl::debug;
+	f[1] = &Harl::info;
+	f[2] = &Harl::warning;
+	f[3] = &Harl::error;
+}
+
+Harl::~Harl()
+{
+	//destructor
+}
+
+Harl	&Harl::operator=(const Harl &src) {
+	if (this != &src)
+	{
+		for (int i = 0; i < 4; i++)
+		{
+		   this->levels[i] = src.levels[i];
+		   this->f[i] = src.f[i];
+		}
+	}
+	return *this;
+}
+
+void	Harl::complain( std::string level )
+{
+	for (int i = 0; i < 4; i++)
+		if (levels[i] == level)
+			(this->*f[i])();
+	return ;
 }
