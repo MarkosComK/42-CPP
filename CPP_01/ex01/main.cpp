b58870b41231d2b373b7134a1800d5efa5a27db7
MarkosComK <markoscomks@gmail.com>
2024-12-02 18:48:35 +0000

    Files and flag fixes

--- CPP_01/ex01/main.cpp
+++ CPP_01/ex01/main.cpp
@@ -1,15 +1,6 @@
 #include "Zombie.hpp"
 
-Zombie* zombieHorde( int N, std::string name )
-{
-	//create a arr[] of zombies
-	Zombie*	horde = new Zombie[N];
-
-	for (int i = 0; i < N; i++)
-		horde[i].setName(name);
-
-	return (horde);
-}
+Zombie* zombieHorde( int N, std::string name );
 
 int	main(void)
 {
