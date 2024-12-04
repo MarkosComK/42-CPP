3172b28092cc2b4f777a2cb91ca4de3a2b909d4e
MarkosComK <markoscomks@gmail.com>
2024-12-04 17:25:28 +0000

    Created new constructor for no weapon

--- CPP_01/ex03/HumanB.cpp
+++ CPP_01/ex03/HumanB.cpp
@@ -1,5 +1,12 @@
 #include "HumanB.hpp"
 
+HumanB::HumanB(std::string name, Weapon* weapon = NULL) : _name(name), _weapon(weapon){
+	if (weapon == NULL)
+		std::cout << _name << " is unarmed " << std::endl;
+	else
+		std::cout << _name << " Has received a " << BLUE << _weapon->getType() << RESET << std::endl;
+}
+
 HumanB::HumanB(std::string name) : _name(name)
 {
 	std::cout << _name << " is unarmed " << std::endl;
