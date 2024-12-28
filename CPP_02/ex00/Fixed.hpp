056e3bb622377b6cdf5c7ffe1f131dd929680eaa
Markos Santos <msantos@devexperts.com>
2024-12-28 12:12:17 +0000

    Class itself

--- CPP_02/ex00/Fixed.hpp
+++ CPP_02/ex00/Fixed.hpp
@@ -1,21 +1,24 @@
 #ifndef FIXED_HPP
 #define FIXED_HPP
+#include <iostream>
 
 class Fixed
 {
 	private:
-		int					point;
-		static const int	fraction;
+		int					_point;
+		static const int	_fraction;
 
 	public:
-		//Default constructor
-		Fixed();
-		//Copy constructor
-		//Copy assignment operator overload
-		//Default destructor
-		~Fixed();
-		int		getRawBits( void );
+		int		getRawBits( void ) const;
 		void	setRawBits( int const raw );
+		//Default constructor
+		Fixed(void);
+		//Copy constructor
+		Fixed(const Fixed& src);
+		//Copy assignment operator overload
+		Fixed& operator=(const Fixed& rhs);
+		//Default destructor
+		~Fixed(void);
 
 };
 
