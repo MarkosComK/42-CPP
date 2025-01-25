da0e81a6eb558bf7f4814b3f7bc0ba73a5e978aa
MarkosComK <markoscomks@gmail.com>
2025-01-25 18:30:35 +0000

    thats it i think

--- CPP_04/ex03/AMateria.hpp
+++ CPP_04/ex03/AMateria.hpp
@@ -5,16 +5,16 @@
 #include "ICharacter.hpp"
 
 class AMateria {
-protected:
-    std::string type;
+	protected:
+		std::string type;
 
-public:
-    AMateria(std::string const & type);
-    virtual ~AMateria();
-    
-    std::string const & getType() const;
-    virtual AMateria* clone() const = 0;
-    virtual void use(ICharacter& target);
+	public:
+		AMateria(std::string const & type);
+		virtual ~AMateria();
+
+		std::string const & getType() const;
+		virtual AMateria* clone() const = 0;
+		virtual void use(ICharacter& target);
 };
 
 #endif
